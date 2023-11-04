ed4b9dc84f7ea7f8fed7e7a1f9151dcfd7b3499d
Vasundhara Sharma <65170222+sharma-vasundhara@users.noreply.github.com>
2023-11-04 11:54:43 -0400

    updated requirements

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 st.set_page_config(layout="wide", page_title="INFO7374: Algorithmic Marketing")
 st.title("INFO7374: Assignment 3 - Variational Auto Encoder")
-#st.subheader("Group 1 (Team 2): Adit Bhosale, Sowmya Chatti, Vasundhara Sharma")
+st.subheader("Group 1 (Team 2): Adit Bhosale, Sowmya Chatti, Vasundhara Sharma")
 
 st.header("Visual Search Using Variational Autoencoders")
 
