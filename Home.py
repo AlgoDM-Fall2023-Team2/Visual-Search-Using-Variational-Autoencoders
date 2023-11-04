8102d00ba1e604f0b63b2f54c1459d2e585fb1e4
Vasundhara Sharma <65170222+sharma-vasundhara@users.noreply.github.com>
2023-11-04 10:45:22 -0400

    changed title

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ ds = tfp.distributions
 from PIL import Image
 
 st.set_page_config(layout="wide", page_title="INFO7374: Algorithmic Marketing")
-st.title("INFO7374: Assignment 1")
+st.title("INFO7374: Assignment 3 - VAE")
 st.subheader("Group 1 (Team 2): Adit Bhosale, Sowmya Chatti, Vasundhara Sharma")
 
 st.header("Visual Search Using Variational Autoencoders")
